ea1370f2aba037f75f15f6240a2ecd7e83290a5b
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-05 19:47:37 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :items, :price, :quantity
+
+  def initialize(employee_discount = 0)
+    @total = 0
+    @discount = employee_discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    @price = price
+    @quantity = quantity
+    counter = 0
+    while (counter < quantity)
+      @items << title
+      counter += 1
+    end
+    @total += price*quantity
+  end
+
+  def apply_discount
+    @total -= @total*@discount*0.01
+    puts "After the discount, the total comes to $#{@total}."
+  end
+
+  def void_last_transaction
+
+  end
+
+end
